f96900197af4e16415251f17eea61c6d53737968
mymacbook <1017962047@qq.com>
2019-05-13 09:57:05 +0800

    log测试

--- advanced/common/config/params.php
+++ advanced/common/config/params.php
@@ -9,9 +9,9 @@ return [
         'column'=>array(
             //'首页'=>array('mark'=>'home','child'=>array()),
             '腾讯'=>array('mark'=>'qq','child'=>array(
-                array('name'=>'刺激战场','mark'=>'qq_pg'),
                 array('name'=>'王者荣耀','mark'=>'qq_pvp'),
-                array('name'=>'和平精英','mark'=>'qq_gp')
+                array('name'=>'和平精英','mark'=>'qq_gp'),
+                array('name'=>'刺激战场','mark'=>'qq_pg')
             )),
             '网易'=>array('mark'=>'wy','child'=>array(
                 array('name'=>'率土之滨','mark'=>'wy_stzb'),
