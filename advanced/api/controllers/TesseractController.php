69f92f30a97b6b50809291ec7a97f93e554ba9e6
mymacbook <1017962047@qq.com>
2019-06-13 11:38:04 +0800

    Tesseract

--- advanced/api/controllers/TesseractController.php
+++ advanced/api/controllers/TesseractController.php
@@ -21,16 +21,27 @@ class TesseractController extends SiteController
     public function actionIdentify(){
 
 
-
+        $data=Yii::$app->request->post();
 
         list($msec, $sec) = explode(' ', microtime());
         $msectime = (float)sprintf('%.0f', (floatval($msec) + floatval($sec)) * 1000);
         $string = strrev($_FILES["img"]["name"]);
         $array = explode('.',$string);
         $hz=strrev($array[0]);
+        $mg_hz_arr=array('png','jpeg','jpg');
+        if (!in_array($hz,$mg_hz_arr)){
+            return Msg::message($mg_hz_arr, -4, "目前支持图片!");
+        }
+        if (empty($data['ztk']) || !isset($data['ztk'])){
+            $data['ztk']='eng';
+        }
+        $ztk=array('chi_sim','eng');
+        if (!in_array($data['ztk'],$ztk)){
+            return Msg::message($ztk, -4, "目前支持字体库!");
+        }
 
 
-       // $data=Yii::$app->request->post();
+
         $filename ='update/tesser/'.$msectime.'.'.$hz;
         move_uploaded_file($_FILES["img"]["tmp_name"],$filename);
 
@@ -38,7 +49,11 @@ class TesseractController extends SiteController
         $path=str_ireplace('controllers','web/',__DIR__);
         $txt_path=$path.'update/txt/';
 
-        passthru("tesseract ".$path.$filename." ".$txt_path.$msectime."  -l chi_sim");
+        if ($data['ztk']=='eng') {
+            passthru("tesseract " . $path . $filename . " " . $txt_path . $msectime);
+        }else{
+            passthru("tesseract " . $path . $filename . " " . $txt_path . $msectime . "  -l ".$data['ztk']);
+        }
         $code = file_get_contents($txt_path.$msectime.".txt");
         //删除文件
         unlink( $path.$filename);
