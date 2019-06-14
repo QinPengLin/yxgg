3ccd209cacd2956fc49b99da6ea5ba8c04de599d
mymacbook <1017962047@qq.com>
2019-06-14 11:10:46 +0800

    Tesseract

--- advanced/api/views/layouts/main.php
+++ advanced/api/views/layouts/main.php
@@ -18,7 +18,7 @@ AppAsset::register($this);
 <head>
     <meta charset="<?= Yii::$app->charset ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
     <?php $this->registerCsrfMetaTags() ?>
     <title><?= Html::encode($this->title) ?></title>
     <?php $this->head() ?>
